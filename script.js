df7463a78e7afd2fb1a8653b3eabe059ce5262ad
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2024-08-27 22:53:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 function convertToRoman(num) {
-  	const obj = {
-      'M' :1000, 
-      'D' : 500, 
-      'C' : 100, 
-      'L' : 50, 
-      'X' : 10, 
-      'V' : 5, 
-      'I' : 1
-    };
+  const obj = {
+    'M': 1000,
+    'CM': 900,
+    'D': 500,
+    'CD': 400,
+    'C': 100,
+    'XC': 90,
+    'L': 50,
+    'XL': 40,
+    'X': 10,
+    'IX': 9,
+    'V': 5,
+    'IV': 4,
+    'I': 1
+  };
 
-	let roman = ""
-  for ( i in obj ) {
-    while ( num >= obj[i] ) {
+  let roman = "";
+  for (let i in obj) {
+    while (num >= obj[i]) {
       roman += i;
       num -= obj[i];
     }
@@ -20,5 +26,4 @@ function convertToRoman(num) {
 }
 
 console.log(convertToRoman(11));
-module.exports = convertToRoman
-
+module.exports = convertToRoman;
